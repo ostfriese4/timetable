f9898f18e26ba0eb08c86e845ac3127219e4cb11
Jonas Harer <jonas.harer@icloud.com>
2026-03-08 21:48:16 +0100

    remove unnecessary comment

--- src/main.py
+++ src/main.py
@@ -71,14 +71,6 @@ class UntisApplication(Adw.Application):
         self.props.active_window.login_window.present(self.props.active_window)
 
     def create_action(self, name, callback, shortcuts=None):
-        """Add an application action.
-
-        Args:
-            name: the name of the action
-            callback: the function to be called when the action is
-              activated
-            shortcuts: an optional list of accelerators
-        """
         action = Gio.SimpleAction.new(name, None)
         action.connect("activate", callback)
         self.add_action(action)
