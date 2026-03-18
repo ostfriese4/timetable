fa9697738ae04ad61cb2eb48cff449652ea94641
Jonas Harer <jonas.harer@icloud.com>
2026-03-18 16:57:53 +0100

    fixed fetch homework

--- src/homework_api.py
+++ src/homework_api.py
@@ -1,18 +1,21 @@
 from .api import api
 import json
 from webuntis.utils.remote import rpc_request
+from .credentials import getCredentials
 
 def fetchHomeworks(start, end):
     session = api.login()
 
     jsessionid = session.config["jsessionid"]
     useragent = session.config["useragent"]
-    server = session.config["server"]
+    server = getCredentials()["server"]
     school = session.config["school"]
 
     s = session.config["_http_session"]
 
     url = server + "/WebUntis/api/homeworks/lessons?startDate=" + start.strftime("%Y%m%d") + "&endDate=" + end.strftime("%Y%m%d") + "&school=" + school
+    if not url.startswith("https://"):
+        url = "https://" + url
 
     print(url)
 
@@ -26,10 +29,6 @@ def fetchHomeworks(start, end):
     result = r.text
 
     print(result)
-
-    parameters = session._create_date_param(end, start, id=5, type="student")
-    data = rpc_request(session.config, "getHomework", parameters)
-    print(data)
 
     session.logout()
 
