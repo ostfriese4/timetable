e6a9fbbc459bc18f113cfa5609b7416b01b86acb
Jonas Harer <jonas.harer@icloud.com>
2026-03-18 12:08:00 +0100

    fetch data

--- src/homework_api.py
+++ src/homework_api.py
@@ -1,4 +1,6 @@
 from .api import api
+import json
+from webuntis.utils.remote import _request_getid, _parse_result, _send_request
 
 def fetchHomeworks(start, end):
     api.login()
@@ -7,8 +9,23 @@ def fetchHomeworks(start, end):
     jsessionid = session.config["jsessionid"]
     useragent = session.config["useragent"]
     server = session.config["server"]
+    school = session.config["school"]
 
-    url = server + "/WebUntis/api/homeworks/lessons?startDate=" + start.strftime("%Y%m%d") + "&endDate=" + end.strftime("%Y%m%d")
+    session = session.config["_http_session"]
+
+    url = server + "/WebUntis/api/homeworks/lessons?startDate=" + start.strftime("%Y%m%d") + "&endDate=" + end.strftime("%Y%m%d") + "&school=" + school
+
     print(url)
+
+    headers = {
+        u'User-Agent': useragent,
+        u'Content-Type': u'application/json'
+    }
+    headers['Cookie'] = u'JSESSIONID=' + jsessionid
+
+    r = session.get(url, data=json.dumps({}), headers=headers)
+    result = r.text
+
+    print(result)
 
     api.logout()
