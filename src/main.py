02052847f47a0816273ac2e437ddf925391593d5
Jonas Harer <jonas.harer@icloud.com>
2026-03-19 08:55:31 +0100

    release 2.0

--- src/main.py
+++ src/main.py
@@ -50,7 +50,7 @@ class UntisApplication(Adw.Application):
         about = Adw.AboutDialog(application_name=_('Timetable'),
                                 application_icon='page.codeberg.ostfriese4.Untis',
                                 developer_name='Ostfriese4',
-                                version='1.2',
+                                version='2.0',
                                 developers=['Ostfriese4'],
                                 copyright='© 2026 Ostfriese4')
         # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
