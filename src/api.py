51a5c0b90663f098efc96395e1e721f1ee702514
Jonas Harer <jonas.harer@icloud.com>
2026-06-01 21:40:03 +0200

    release 3.0

--- src/api.py
+++ src/api.py
@@ -25,7 +25,7 @@ import traceback
 import webuntis
 from .credentials import getCredentials, setCredentials
 
-version = "devel"
+version = "3.0"
 useragent = "page.codeberg.ostrfiese4.Untis " + version
 
 class untisApi:
