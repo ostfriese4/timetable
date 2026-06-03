206a3f4cb4c4722f0cf94636c0fa9732a840910b
Jonas Harer <jonas.harer@icloud.com>
2026-06-03 12:42:19 +0200

    build dependecies with runtime

--- update-pip.sh
+++ update-pip.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/pip/flatpak-pip-generator.py
-python flatpak-pip-generator.py --requirements-file='pypi.txt' --output pypi.json
+python flatpak-pip-generator.py --requirements-file='pypi.txt' --output pypi.json --runtime='org.gnome.Sdk//50'
 rm flatpak-pip-generator.py
