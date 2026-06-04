560186f421904168b7f76a90a737c96e51d8c43a
Jonas Harer <jonas.harer@icloud.com>
2026-06-04 18:29:18 +0200

    update helper script

--- update-pip.sh
+++ update-pip.sh
@@ -3,3 +3,6 @@
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/pip/flatpak-pip-generator.py
 python flatpak-pip-generator.py --requirements-file='pypi.txt' --output pypi.json --runtime='org.gnome.Sdk//50'
 rm flatpak-pip-generator.py
+
+git add pypi.json
+git commit -m "updated dependencies"
