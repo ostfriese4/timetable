ea1daa9abcec7bf25daa78c9b103790d7db99b6f
Felitendo <felitendo@noreply.codeberg.org>
2026-07-20 18:05:04 +0200

    show the substitution text of a lesson (#23)
    Shows the substitution text (`substText`) of a lesson in the lesson dialog, e.g. notes like a room change that are entered with a substitution.
    
    Co-authored-by: Jonas Harer <jonas.harer@icloud.com>
    Reviewed-on: https://codeberg.org/ostfriese4/untis/pulls/23

--- src/information.py
+++ src/information.py
@@ -49,6 +49,8 @@ class InformationWindow(Adw.Dialog):
         data[_("Room")] = lesson["room"]
         data[_("Room description")] = lesson["room-info"]
         data[_("Teacher")] = lesson["teachers-long"]
+        if lesson.get("substText"):
+            data[_("Substitution")] = lesson["substText"]
         if lesson["lessonInfo"] is not None:
             data[_("Information about this lesson")] = lesson["lessonInfo"]
         if lesson["teachingContent"] is not None:
